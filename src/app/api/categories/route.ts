23c891cd36e3f04cee59dde3cd680a3d84d96c3c
Kamrul Hasan <kamrul444216@gmail.com>
2023-09-11 02:09:51 +0600

    fixed categories issue

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+
+import { prisma } from "@/utils/connect";
 import { NextResponse } from "next/server";
-const prisma = new PrismaClient();
+
 // fetch categories
 export const GET = async () => {
   try {
